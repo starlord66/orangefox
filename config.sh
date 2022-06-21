04bb0a56ee560e083ee5d112d5dd7bfc99351de6
TheArtyomMDev <93202601+TheArtyomMDev@users.noreply.github.com>
2022-06-21 12:49:49 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/nebrassy/device_xiaomi_sweet-TWRP -b android-12.1"
 
-export DEVICE="beryllium"
+export DEVICE="sweet"
 export OEM="xiaomi"
 
 # Build Target
